b9bd54b594f7a39a873947d58cd0e403843fa4a1
mosmmoses <>
2023-11-15 16:08:54 +0000

    HW_10 fixed

--- HW_10/hw10_customer_2.cpp
+++ HW_10/hw10_customer_2.cpp
@@ -8,44 +8,79 @@ class Person
   string surname;
   string address;
   unsigned card_number;
-  //unsigned bank_account;
+  unsigned bank_account;
   
   public:
-  Person(string p_name, unsigned p_card_number)
+  Person(string p_name, string p_surname, string p_address, unsigned p_card_number, unsigned p_bank_account)
   { 
     name = p_name;
+    surname = p_surname;
+    address = p_address;
     card_number = p_card_number;
+    bank_account = p_bank_account;
   }
   
   void print() 
   {
-    cout << "Name: " << name << "\tCard: " << card_number << endl;
+    cout << "Person: " << surname << " " << name << "\tCard number: " << card_number << endl;
   }
-  
+
+  void setName(string p_name) 
+  { 
+    name = p_name;
+  }
+
+  void setSurname(string p_surname){
+    surname = p_surname;
+  }
+
+  void setAddress(string p_address){
+    address = p_address;
+  }
+
   void setCard(unsigned p_card_number) 
   { 
     card_number = p_card_number;
+  }
+
+  void setBank(unsigned p_bank_account){
+    bank_account = p_bank_account;
   }
 
   string getName()
   {
     return name;
   }
-  
-  unsigned getCard()
+
+  string getSurame()
   {
+    return surname;
+  }
+
+  string getAddress(){
+    return address;
+  }
+  
+  unsigned getCard(){
     return card_number;
   }
+
+  unsigned getBank(){
+    return bank_account;
+  }
+
 };
 
 int main()
 {
-    Person moses{"Moses", "Moss", "Miracle st. 123", 123456789};
-    
+    Person moses{"Moses", "Moss", "Miracle st. 123", 123456789, 1212121212};
+    //Person moses{"Moses", "Moss", 123456789};
     // изменяем номер карты
     moses.setCard(987654321);
     moses.print(); // Name: Moses   Card: 987654321
 
-    //отдельно получаем имя
-    cout << "Person name: " << moses.getName() << endl;
+    //отдельно получаем фамилию
+    cout << "Person's surname: " << moses.getSurame() << endl;
+    cout << "Addres: " << moses.getAddress() << endl;
+    cout << "Bank account: " << moses.getBank() << endl;
 }
